7d9fd1f9cbb3d25667cb672d21d9d9161c8b018a
eikyuu <v.duguet.dev@gmail.com>
2024-05-18 21:30:09 +0200

    feat: add ChatGptService code for converting Vue.js code to API composition

--- app/services/ChatGptService.ts
+++ app/services/ChatGptService.ts
@@ -12,19 +12,19 @@ class ChatGptService {
         
         const response = await axios.post(
             'https://api.openai.com/v1/chat/completions',
-            {
-              "model": "gpt-3.5-turbo-0125",
-              "messages": [
+              {
+                "model": "gpt-3.5-turbo-0125",
+                "messages": [
                   {
-                      "role": "system",
-                      "content": "you're an expert in web development with vuejs .you'll only receive the vuejs code with the api option you'll have to convert to api composition with the typescript setup <script setup> don't forget the props with the interfaces. give me only the code converted to response no text from you.format the code as directly usable vuejs code."
+                    "role": "system",
+                    "content": "tu es un expert en développement web avec vuejs et nuxtjs. tu dois convertir du code vuejs ou nuxtjs avec API options en API composition. exemple de code converti  en API composition:  `<script setup lang='ts'> import { ref, onMounted, watch  } from 'vue'    export interface Props {   msg?: string   labels?: string[]   title?: String   likes?: Number   count?: Number }  const props = withDefaults(defineProps<Props>(), {   msg: 'hello',   labels: () => ['one', 'two'],   count: 0,   title: 'toto' })  const message = ref('Hello World!') const count = ref(props.count)  onMounted(() => {   message.value = 'on mounted' })    watch(count, (newCount) => {   // yes, console.log() is a side effect   console.log(`new count is: ${newCount}`) }) </script>  <template>   <h1>{{ message }}</h1>   <h1>{{count}}</h1>   <p>{{title}}</p> </template>`  base toi sur l'exemple pour convertir code, n'oublie pas l'interface ni les props et le withDefaults si une props a une valeur par defaut. donne moi seulement le code converti en réponse pas de texte de ta part juste le code a copier coller directement."
                   },
                   {
-                      "role": "user",
-                      "content": prompt
+                    "role": "user",
+                    "content": prompt
                   }
-              ]
-          }
+                ]
+              }
             ,
             {
               headers: {
@@ -43,3 +43,5 @@ class ChatGptService {
 }
 
 export default new ChatGptService();
+
+
